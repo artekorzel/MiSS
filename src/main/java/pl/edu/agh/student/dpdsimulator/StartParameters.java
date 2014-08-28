63ca4114807f3626423cd651a88d0f61a345d4bb
Artur Orzeł <artekorzel@gmail.com>
2014-08-28 21:20:18 +0200

    fix of generating velocities

--- src/main/java/pl/edu/agh/student/dpdsimulator/StartParameters.java
+++ src/main/java/pl/edu/agh/student/dpdsimulator/StartParameters.java
@@ -3,9 +3,9 @@ package pl.edu.agh.student.dpdsimulator;
 public interface StartParameters {
 
     int numberOfSteps = 100;
-    int numberOfDroplets = 1000;
+    int numberOfDroplets = 30000;
     float deltaTime = 1.0f;
-    float cutoffRadius = 0.99f;
+    float cutoffRadius = 0.1f;
     float boxSize = 26.0f;
     float temperature = 310.0f;
     float boltzmanConstant = 1 / temperature;
