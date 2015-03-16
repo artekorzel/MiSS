c897090dcafc71c168c7d6b8f5450c9d25775e46
fbuszko <fbuszko@infusion.com>
2015-03-16 13:27:05 +0100

    changed plain file to properties file

--- src/main/java/pl/edu/agh/student/dpdsimulator/Simulation.java
+++ src/main/java/pl/edu/agh/student/dpdsimulator/Simulation.java
@@ -1,11 +1,14 @@
 package pl.edu.agh.student.dpdsimulator;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bridj.Pointer;
@@ -22,8 +25,8 @@ public abstract class Simulation {
     public static int numberOfSteps;    
     public static float deltaTime;
     
-    public static float initBoxSize = 8f;
-    public static float initBoxWidth = initBoxSize;
+    public static float initBoxSize;
+    public static float initBoxWidth;
     
     public static float boxSize;
     public static float boxWidth;
@@ -37,7 +40,9 @@ public abstract class Simulation {
     
     public static int numberOfCells;
     public static int numberOfDroplets;
+    public static float numberOfDropletsFactor;
     public static int maxDropletsPerCell;
+    public static int averageDropletsPerCell;
     public static int numberOfCellKinds;
     
     protected Pointer<Dpd.DropletParameters> dropletParametersPointer;
@@ -65,40 +70,43 @@ public abstract class Simulation {
     
     protected void loadInitialDataFromFile(String filename){
         try{
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            Properties prop = getProperties();
 
-            shouldStoreFiles = getBoolean(bufferedReader.readLine());
-            numberOfSteps = getInt(bufferedReader.readLine());
-            deltaTime = getFloat(bufferedReader.readLine());
-            cellRadius = getFloat(bufferedReader.readLine());
-            boxSizeScale = getFloat(bufferedReader.readLine());
-            boxWidthScale = getFloat(bufferedReader.readLine());
+            shouldStoreFiles = Boolean.parseBoolean(prop.getProperty("shouldStoreFiles"));
+            numberOfSteps = Integer.parseInt(prop.getProperty("numberOfSteps"));
+            deltaTime = Float.parseFloat(prop.getProperty("deltaTime"));
+            cellRadius = Float.parseFloat(prop.getProperty("cellRadius"));
+            initBoxSize = Float.parseFloat(prop.getProperty("initBoxSize"));
+            initBoxWidth = Float.parseFloat(prop.getProperty("initBoxWidth"));
+            boxSizeScale = Float.parseFloat(prop.getProperty("boxSizeScale"));
+            boxWidthScale = Float.parseFloat(prop.getProperty("boxWidthScale"));
+            maxDropletsPerCell = Integer.parseInt(prop.getProperty("maxDropletsPerCell"));
+            averageDropletsPerCell = Integer.parseInt(prop.getProperty("averageDropletsPerCell"));
+            numberOfDroplets = Integer.parseInt(prop.getProperty("numberOfDroplets"));
             
             baseNumberOfCells = (int) (Math.ceil(2 * initBoxSize / cellRadius) * Math.ceil(2 * initBoxSize / cellRadius) * Math.ceil(2 * initBoxWidth / cellRadius));
-            baseNumberOfDroplets = baseNumberOfCells * 4;
-            maxDropletsPerCell = (baseNumberOfDroplets / baseNumberOfCells) * 5 + 1;            
-            numberOfDroplets = baseNumberOfDroplets / 8;
+            baseNumberOfDroplets = baseNumberOfCells * averageDropletsPerCell;
         } catch (Exception e){
             e.printStackTrace();
         }
     }
-    
+
     protected void loadParametersFromFile(String filename){        
         float[] mass, lambda;
         float[][] cutOffRadius, pi, gamma, sigma;
         try {                              
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            Properties prop = getProperties();
             
-            numberOfCellKinds = getInt(bufferedReader.readLine());
+            numberOfCellKinds = Integer.parseInt(prop.getProperty("numberOfCellKinds"));
             
             mass = new float[numberOfCellKinds];
             for(int i = 0; i < numberOfCellKinds; i++){
-                mass[i] = getFloat(bufferedReader.readLine());
+                mass[i] = Float.parseFloat(prop.getProperty("mass(" + i + ")"));
             }
             
             lambda = new float[numberOfCellKinds];
             for(int i = 0; i < numberOfCellKinds; i++){
-                lambda[i] = getFloat(bufferedReader.readLine());
+                lambda[i] = Float.parseFloat(prop.getProperty("lambda(" + i + ")"));
             }
                         
             dropletParametersPointer = Pointer.allocateArray(Dpd.DropletParameters.class, numberOfCellKinds);
@@ -111,28 +119,28 @@ public abstract class Simulation {
             cutOffRadius = new float[numberOfCellKinds][numberOfCellKinds];
             for(int i = 0; i < numberOfCellKinds; i++){
                 for(int j = i; j < numberOfCellKinds; j++){
-                    cutOffRadius[i][j] = cutOffRadius[j][i] = getFloat(bufferedReader.readLine());
+                    cutOffRadius[i][j] = cutOffRadius[j][i] = Float.parseFloat(prop.getProperty("cutoffRadius(" + i + "," + j + ")"));
                 }
             }
             
             pi = new float[numberOfCellKinds][numberOfCellKinds];
             for(int i = 0; i < numberOfCellKinds; i++){
                 for(int j = i; j < numberOfCellKinds; j++){
-                    pi[i][j] = pi[j][i] = getFloat(bufferedReader.readLine());
+                    pi[i][j] = pi[j][i] = Float.parseFloat(prop.getProperty("pi(" + i + "," + j + ")"));
                 }
             }
                         
             gamma = new float[numberOfCellKinds][numberOfCellKinds];
             for(int i = 0; i < numberOfCellKinds; i++){
                 for(int j = i; j < numberOfCellKinds; j++){
-                    gamma[i][j] = gamma[j][i] = getFloat(bufferedReader.readLine());
+                    gamma[i][j] = gamma[j][i] = Float.parseFloat(prop.getProperty("gamma(" + i + "," + j + ")"));
                 }
             }
             
             sigma = new float[numberOfCellKinds][numberOfCellKinds];
             for(int i = 0; i < numberOfCellKinds; i++){
                 for(int j = i; j < numberOfCellKinds; j++){
-                    sigma[i][j] = sigma[j][i] = getFloat(bufferedReader.readLine());
+                    sigma[i][j] = sigma[j][i] = Float.parseFloat(prop.getProperty("sigma(" + i + "," + j + ")"));
                 }
             }
             
@@ -150,6 +158,18 @@ public abstract class Simulation {
         }
     }
 
+    private Properties getProperties() throws IOException {
+        Properties prop = new Properties();
+        String propFileName = "simulation.data";
+        InputStream inputStream = new FileInputStream(propFileName);
+        if (inputStream != null) {
+            prop.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+        return prop;
+    }
+    
     private boolean getBoolean(String line) {
         String result = line.split(":")[1].replaceAll("\\s", "");
         return Boolean.parseBoolean(result);
